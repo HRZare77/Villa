275fa80d5d67df89a12f207c53ff5b8b44a46d95
zare <www.hamidreza.zare1998@gmail.com>
2025-04-09 11:34:01 +0330

    جلسه 4

--- Villa/Controllers/VillaAPIController.cs
+++ Villa/Controllers/VillaAPIController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Villa;
 
 namespace Villa.Controllers
 {
@@ -7,5 +8,14 @@ namespace Villa.Controllers
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Models.Villa> GetVillas()
+        {
+            return new List<Models.Villa>
+            {
+                new Models.Villa { Id = 1, Name = "Villa 1" },
+                new Models.Villa { Id = 2, Name = "Villa 2" }
+            };
+        }
     }
 }
