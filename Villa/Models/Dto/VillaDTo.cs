5588bfae893b797f9e8ccfae3c14d208fd1cf6be
zare <www.hamidreza.zare1998@gmail.com>
2025-04-20 11:47:23 +0330

    فصل 3 قسمت 5

--- Villa/Models/Dto/VillaDTo.cs
+++ Villa/Models/Dto/VillaDTo.cs
@@ -1,8 +1,12 @@
-﻿namespace Villa.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.Models.Dto
 {
     public class VillaDTo
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
