1fd5952e8d091ee5bcb8ae110fc862531b85387b
zare <www.hamidreza.zare1998@gmail.com>
2025-04-25 09:11:30 +0330

    فصل 6 جلسه 1

---
+++ Villa/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Villa.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<IEnumerable<Models.Villa>> GetAllAsync(Expression<Func<Models.Villa, bool>>? filter = null);
+        Task<Models.Villa> GetAsync(Expression<Func<Models.Villa, bool>> filter = null);
+        Task CreateAsync(Models.Villa entity);
+        Task RemoveAsync(Models.Villa entity);
+        Task SaveAsync();
+        Task UpdateAsync(Models.Villa entity);
+    }
+}
