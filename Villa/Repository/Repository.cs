92664c5bf7f8275f87404449b5cc3d186673cccc
zare <www.hamidreza.zare1998@gmail.com>
2025-04-25 12:11:12 +0330

    فصل 6 جلسه 4

---
+++ Villa/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Linq;
+using Villa.Data;
+using Villa.Repository.IRepository;
+
+namespace Villa.Repository
+{
+    public class Repository<T>:IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            _dbSet = db.Set<T>();
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.ToListAsync();
+        }
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = _dbSet;
+            if (!tracked)
+                query = query.AsNoTracking();
+            if (filter != null)
+                query = query.Where(filter);
+
+            return await query.FirstOrDefaultAsync();
+        }
+        public async Task RemoveAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await SaveAsync();
+        }
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await SaveAsync();
+        }
+    }
+}
--- Villa/Repository/VillaRepository.cs
+++ Villa/Repository/VillaRepository.cs
@@ -5,49 +5,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Villa.Repository
 {
-    public class VillaRepository: IVillaRepository
+    public class VillaRepository:Repository<Models.Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db):base(db)
         {
             _db = db;
         }
-        public async Task CreateAsync(Models.Villa entity)
+        
+        public async Task<Models.Villa> UpdateAsync(Models.Villa entity)
         {
-           await _db.Villas.AddAsync(entity);
-            await SaveAsync();
-        } 
-        public async Task<IEnumerable<Models.Villa>> GetAllAsync(Expression<Func<Models.Villa, bool>>? filter = null)
-        {
-            IQueryable<Models.Villa> query = _db.Villas;
-            if (filter != null)
-              query= query.Where(filter);
-
-            return await query.ToListAsync();
-        }
-        public async Task<Models.Villa> GetAsync(Expression<Func<Models.Villa, bool>> filter = null, bool tracked=true)
-        {
-            IQueryable<Models.Villa> query = _db.Villas;
-            if (!tracked)
-                query = query.AsNoTracking();
-            if (filter != null)
-                query = query.Where(filter);
-
-            return await query.FirstOrDefaultAsync();
-        } 
-        public async Task RemoveAsync(Models.Villa entity)
-        {
-            _db.Villas.Remove(entity);
-            await SaveAsync();
-        }
-        public async Task SaveAsync()
-        {
-             await _db.SaveChangesAsync();
-        }
-        public async Task UpdateAsync(Models.Villa entity)
-        {
+            entity.UpdatedDate = DateTime.Now;
             _db.Villas.Update(entity);
-            await SaveAsync();
+            await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
