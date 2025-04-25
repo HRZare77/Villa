ed4f208b5c7e8bf472c7152659ae106692cf55f0
zare <www.hamidreza.zare1998@gmail.com>
2025-04-25 13:11:10 +0330

    فصل 6 جلسه 7

---
+++ Villa/Models/Dto/VillaNumberDTo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.Models.Dto
+{
+    public class VillVillaNumberDToaDTo
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+
+    }
+}
---
+++ Villa/Models/Dto/VillaNumberUpdateDTo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.Models.Dto
+{
+    public class VillaNumberUpdateDTo
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+
+    }
+}
