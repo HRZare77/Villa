d9e2a169fad7675d79ad7be384e8372913d28f8a
zare <www.hamidreza.zare1998@gmail.com>
2025-04-29 09:01:04 +0330

    فصل 7 جلسه 2

---
+++ Villa_Web/Models/APIResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Villa_Web.Models
+{
+    public class APIResponse
+    {
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public bool IsSuccess { get; set; } = true;
+        public object? Result { get; set; }
+        public List<string>? Errors { get; set; }
+        public APIResponse()
+        {
+            Errors = new List<string>();
+        }
+    }
+}
---
+++ Villa_Web/Models/Dto/VillaCreateDTo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa_Web.Models.Dto
+{
+    public class VillaCreateDTo
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public string Amenity { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        [Required]
+        public int Sqft { get; set; }
+
+
+    }
+}
