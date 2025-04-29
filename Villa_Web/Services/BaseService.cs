81de970bde48b706fc83322cad7d037371f2b99a
zare <www.hamidreza.zare1998@gmail.com>
2025-04-29 09:32:47 +0330

    فصل 7 جلسه 4و5

---
+++ Villa_Web/Services/BaseService.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using Villa_Utility;
+using Villa_Web.Models;
+using Villa_Web.Services.IServices;
+
+namespace Villa_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse ResponseModel { get; set; }
+
+        public IHttpClientFactory HttpClientFactory { get; set; }
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            ResponseModel = new APIResponse();
+            HttpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = HttpClientFactory.CreateClient("VillaAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if (apiRequest.Data!=null)
+                {
+                    message.Content=new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, mediaType: "application/json");
+                }
+
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case SD.ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseModel = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(apiContent);
+                
+                    return apiResponseModel;              
+
+            }
+            catch (Exception e)
+            {
+                var dto = new APIResponse()
+                {
+                    Errors = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false,
+                };
+                var res = Newtonsoft.Json.JsonConvert.SerializeObject(dto);
+                var apiResponseModel = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(res);
+                return apiResponseModel;
+            }
+        }
+        public Task<T> SendAsync<T>(APIRequest apiRequest, string token)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<T> SendAsync<T>(APIRequest apiRequest, string token, bool isAuthorized)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Villa_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Villa_Web.Models;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse ResponseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+        Task<T> SendAsync<T>(APIRequest apiRequest, string token);
+        Task<T> SendAsync<T>(APIRequest apiRequest, string token, bool isAuthorized);
+    }
+}
