2a59f92ce3333132c5a6bed9a82ad236b6d766fb
zare <www.hamidreza.zare1998@gmail.com>
2025-04-29 09:06:34 +0330

    فصل 7 جلسه 3

---
+++ Villa_Web/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Villa_Web.Models;
+using Villa_Web.Models.Dto;
+
+namespace Villa_Web
+{
+    public class MappingConfig: Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Models.Villa, VillaDTo>().ReverseMap();
+            CreateMap<VillaDTo, Models.Villa>().ReverseMap();
+
+            CreateMap<Models.Villa, VillaUpdateDTo>().ReverseMap();
+            CreateMap<Models.Villa, VillaCreateDTo>().ReverseMap();
+
+            CreateMap<Models.VillaNumber, VillaNumberDTo>().ReverseMap();
+            CreateMap<VillaNumberDTo, Models.VillaNumber>().ReverseMap();
+
+            CreateMap<Models.VillaNumber, VillaNumberUpdateDTo>().ReverseMap();
+            CreateMap<Models.VillaNumber, VillaNumberCreateDTo>().ReverseMap();
+        }
+    }
+}
--- Villa_Web/Program.cs
+++ Villa_Web/Program.cs
@@ -1,7 +1,11 @@
+using Villa_Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAutoMapper(typeof(MappingConfig).Assembly);
 
 var app = builder.Build();
 
