c65d0448527c668e9c13beba29d42e8d5a5027c6
zare <www.hamidreza.zare1998@gmail.com>
2025-05-03 13:26:26 +0330

    فصل 7 قسمت 18و19و20

--- Villa_Web/Controllers/HomeController.cs
+++ Villa_Web/Controllers/HomeController.cs
@@ -1,31 +1,30 @@
 using System.Diagnostics;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Villa_Web.Models;
+using Villa_Web.Models.Dto;
+using Villa_Web.Services.IServices;
 
 namespace Villa_Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IVillaService _villaService;
+    private readonly IMapper _mapper;
+    public HomeController(IVillaService villaService, IMapper mapper)
     {
-        _logger = logger;
+        _villaService = villaService;
+        _mapper = mapper;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var list = await _villaService.GetAllAsync<APIResponse>();
+        if (list != null && list.IsSuccess)
+        {
+            var model = _mapper.Map<List<VillaDTo>>(list.Result);
+            return View(model);
+        }
+        return View(new List<VillaDTo>());
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
